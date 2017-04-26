ccbed2f980b8cbf2fe34d3f2ee9a7c7b7c2a38ed
willisdorden <willisdorden@gmail.com>
2017-04-26 19:20:06 -0400

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,25 @@
 var friends = [{
-    name:"",
-    photo:"",
-    scores:""
+    name: "john",
+    photo: "url",
+    score:["3","2","3","2","4","5","2","5","3","2","5","3"]
+},{
+        name: "willis",
+        photo: "url",
+        score:["1","2","4","3","1","5","3","1","5","3","4","2"]
+},{
+        name: "sarah",
+        photo: "url",
+        score:["1","2","2","3","2","4","5","3","3","1","5","2"]
+},{
+        name: "clover",
+        photo: "url",
+        score:["2","4","5","1","4","2","4","3","5","2","1","5"]
+},{
+        name: "matt",
+        photo: "url",
+        score:["1","5","2","4","5","1","3","3","4","3","2","4"]
 }];
+
+
 
 module.exports=friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var friend = require(path.join(__dirname, '../app/data/friends'));
+var friend = require('../app/data/friends');
 
 
 
@@ -11,11 +11,37 @@ app.get('/api/friends', function(req, res){
 
 app.post('/api/date',function(req,res){
     var newFriend = req.body;
-    console.log(newFriend);
+    var closestMatch;
+    for (var i = 0; i < friend.length; i++) {
+        var friendScoreArry = friend[i].score;
+        var diff = 0;
+        for(var j = 0; j<newFriend.score.length; j++){
+            var friendScore = parseInt(friendScoreArry[j]);
+            var newFriendScore = parseInt(newFriend.score[j]);
+                diff += Math.abs(friendScore - newFriendScore);
+        }
+        console.log(diff);
+    }
+
+
     friend.push(newFriend);
-    res.json(newFriend);
+
 });
 };
+
+//var popUp = function() {
+  //  for (var i = 0; i < friend.length; i++) {
+   // var friendArry = parseInt(friend[i].scores);
+    //console.log(friendArry)
+      //  var nFriend = parseInt(newFriend.scores);
+        //var diff = 0;
+       // for(var j=0; j<friendArry.length; j++){
+
+
+    //    }
+    //}
+
+//};
 
 
 
