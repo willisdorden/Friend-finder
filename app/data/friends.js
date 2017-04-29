2d9b56566f5db65e99f972577a0c8c48ad74d75d
willisdorden <willisdorden@gmail.com>
2017-04-29 09:49:58 -0400

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [{
     name: "john",
-    photo: "url",
+    photo: "http://www.parisphoto.com/content/committee_members/18/image2/detail/51a4751509d85Timothy_Persons,_Helsinki.jpg",
     score:["3","2","3","2","4","5","2","5","3","2","5","3"]
 },{
         name: "willis",
-        photo: "url",
+        photo: "http://www.cfi-icf.ca/uploads/screenings/679/lg_LostPersonsArea03.jpg",
         score:["1","2","4","3","1","5","3","1","5","3","4","2"]
 },{
         name: "sarah",
-        photo: "url",
+        photo: "http://wrcb.images.worldnow.com/images/15785149_BG3.jpg",
         score:["1","2","2","3","2","4","5","3","3","1","5","2"]
 },{
         name: "clover",
-        photo: "url",
+        photo: "http://i2.ytimg.com/vi/iYqHgcYK0Oo/0.jpg",
         score:["2","4","5","1","4","2","4","3","5","2","1","5"]
 },{
         name: "matt",
-        photo: "url",
+        photo: "http://holobest.com/directorys/wp-content/uploads/2014/08/034.png",
         score:["1","5","2","4","5","1","3","3","4","3","2","4"]
 }];
 
